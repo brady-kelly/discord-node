6f0f7c807ba1f2b08b745fee35daab8fca818a6c
Brady Kelly <brady_kelly@tutamail.com>
2026-01-20 13:50:17 +0200

    Logs in

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,28 +1,15 @@
-import { Client } from "discord.js";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 import { deployCommands } from "./deploy-commands";
 import { commands } from "./commands";
 import { config } from "../config/config";
 
-const client = new Client({
-  intents: ["Guilds", "GuildMessages", "DirectMessages"],
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.once("ready", () => {
-  console.log("Discord bot is ready! 🤖");
-});
-
-client.on("guildCreate", async (guild) => {
-  await deployCommands({ guildId: guild.id });
-});
-
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) {
-    return;
-  }
-  const { commandName } = interaction;
-  if (commands[commandName as keyof typeof commands]) {
-    commands[commandName as keyof typeof commands].execute(interaction);
-  }
-});
-
+// Log in to Discord with your client's token
 client.login(config.DISCORD_TOKEN);
--- src/config/config.ts
+++ src/config/config.ts
@@ -2,13 +2,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { DISCORD_TOKEN, DISCORD_CLIENT_ID } = process.env;
+const { DISCORD_TOKEN } = process.env;
 
-if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID) {
+if (!DISCORD_TOKEN) {
   throw new Error("Missing environment variables");
 }
 
 export const config = {
   DISCORD_TOKEN,
-  DISCORD_CLIENT_ID,
 };
