52c8f6a3beeb574a8bffddbf02eec0f60559aa3d
Brady Kelly <brady_kelly@tutamail.com>
2026-01-20 14:47:59 +0200

    deploy command done

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,12 +2,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { DISCORD_TOKEN } = process.env;
+const { DISCORD_TOKEN, CLIENT_ID, DISCORD_GUILD_ID } = process.env;
 
-if (!DISCORD_TOKEN) {
+if (!DISCORD_TOKEN || !CLIENT_ID || !DISCORD_GUILD_ID) {
   throw new Error("Missing environment variables");
 }
 
 export const config = {
   DISCORD_TOKEN,
+  DISCORD_GUILD_ID,
+  CLIENT_ID,
 };
